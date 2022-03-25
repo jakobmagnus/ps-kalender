0369898aae24ed82b5c22712564ab3a442302716
jakobmagnus <53524961+jakobmagnus@users.noreply.github.com>
2022-03-25 10:31:00 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ const IndexPage = () => (
     <p>
       <Link to="webcal://p107-caldav.icloud.com/published/2/MTAzMTcxNTQ5NDEwMzE3MZwJcqtX7Zo-VjkQqkJu8Nl1OcSZD6bBlse2PZOljUFmihsYJI0sS-PRI5zSQtIIn1CUK2Xy085FbEZooDkLuLk">Tema: Brottsdokumentär</Link> <br />
       <Link to="webcal://p107-caldav.icloud.com/published/2/MTAzMTcxNTQ5NDEwMzE3MZwJcqtX7Zo-VjkQqkJu8NlgKJyV3vEJoEmnDRtI8KJ9wWtjFKfLUWIK3AX7Admn0OcYklg1dW_llO8iYrvaxVY">Tema: Fåglar och natur</Link> <br />
-      <Link to="webcal://p107-caldav.icloud.com/published/2/MTAzMTcxNTQ5NDEwMzE3MZwJcqtX7Zo-VjkQqkJu8NlgKJyV3vEJoEmnDRtI8KJ9wWtjFKfLUWIK3AX7Admn0OcYklg1dW_llO8iYrvaxVY">Tema: Kända och okända excentriker</Link> <br />
+      <Link to="webcal://p107-caldav.icloud.com/published/2/MTAzMTcxNTQ5NDEwMzE3MZwJcqtX7Zo-VjkQqkJu8Nl0IFM7ONoXSnSDDtq8qL1ASzrAgEnHOgP37WFKLYIGCd2QkTBTaETAa0vvU0cyClM">Tema: Kända och okända excentriker</Link> <br />
     </p>
   </Layout>
 )
