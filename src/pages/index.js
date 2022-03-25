ab82a61e33c39eb156424a336e42936406dc8e57
jakobmagnus <53524961+jakobmagnus@users.noreply.github.com>
2022-03-25 13:38:05 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,8 +5,8 @@ import Layout from "../components/layout"
 
 const IndexPage = () => (
   <Layout>
-    <h1>Vill du bli inspirerad?</h1>
-    <p>Få det bästa från SR, SVT och UR direkt i din kalender.</p>
+    <h1>Programtips rakt in i din kalender!</h1>
+    <p>Prenumerera på den bästa blandningen av det just du är intresserad av från SR, SVT och UR direkt i din kalenderapp.</p>
     <p>
       <Link to="webcal://p107-caldav.icloud.com/published/2/MTAzMTcxNTQ5NDEwMzE3MZwJcqtX7Zo-VjkQqkJu8Nl1OcSZD6bBlse2PZOljUFmihsYJI0sS-PRI5zSQtIIn1CUK2Xy085FbEZooDkLuLk">Tema: Brottsdokumentär</Link> <br />
       <Link to="webcal://p107-caldav.icloud.com/published/2/MTAzMTcxNTQ5NDEwMzE3MZwJcqtX7Zo-VjkQqkJu8NlgKJyV3vEJoEmnDRtI8KJ9wWtjFKfLUWIK3AX7Admn0OcYklg1dW_llO8iYrvaxVY">Tema: Fåglar och natur</Link> <br />
